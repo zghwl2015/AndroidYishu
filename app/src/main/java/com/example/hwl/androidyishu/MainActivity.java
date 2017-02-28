3bc2e32556b064d491146d0440e0fea2543bb906
Hwl <hwl@gmail.com>
2017-02-28 21:50:26 +0800

    add the customized drawable

--- app/src/main/java/com/example/hwl/androidyishu/MainActivity.java
+++ app/src/main/java/com/example/hwl/androidyishu/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.hwl.androidyishu;
 
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.ScaleDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
+        setContentView(R.layout.activity_main);
+
+        TextView textView =(TextView) findViewById(R.id.button);
+        TransitionDrawable transitionDrawable =(TransitionDrawable) textView.getBackground();
+        transitionDrawable.startTransition(2000);
+
+        View scale =(View) findViewById(R.id.text_view);
+        ScaleDrawable scaleDrawable =(ScaleDrawable) scale.getBackground();
+        scaleDrawable.setLevel(3000);
+
+        ImageView imageClip =(ImageView) findViewById(R.id.clip);
+        ClipDrawable clipDrawable =(ClipDrawable) imageClip.getDrawable();
+        clipDrawable.setLevel(9000);
     }
 }
